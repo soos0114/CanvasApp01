69ecd2a95b2cca1994145af67108c7b271f345f6
soos0114 <jtn90go@icloud.com>
2019-02-23 16:59:38 +0900

    円書きました

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -3,16 +3,19 @@ package com.example.dojo.canvasapp01;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 
 
 //コンストラクタ（初期設定）
 public class MyView extends View{
-
+private Paint paint;
 
     public MyView(Context con) {
         super(con);
+
+        paint = new Paint();
     }
 
 //描画の処理
@@ -22,5 +25,12 @@ public class MyView extends View{
         //背景
         canvas
         .drawColor(Color.GREEN);
+        //円
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 500, 50,paint);
+        paint.setColor(Color.CYAN);
+        canvas.drawCircle(300, 600, 50,paint);
     }
 }
