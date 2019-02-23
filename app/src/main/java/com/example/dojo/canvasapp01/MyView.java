8bb1d30418bf6284719847ccbdd0b746f49bb0ee
soos0114 <jtn90go@icloud.com>
2019-02-23 16:42:43 +0900

    背景色いれたよ

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -1,6 +1,8 @@
 package com.example.dojo.canvasapp01;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 
@@ -11,5 +13,14 @@ public class MyView extends View{
 
     public MyView(Context con) {
         super(con);
+    }
+
+//描画の処理
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+
+        //背景
+        canvas
+        .drawColor(Color.GREEN);
     }
 }
