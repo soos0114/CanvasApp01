64d5e446c6779112434281a22540d954162b45a5
soos0114 <jtn90go@icloud.com>
2019-02-23 15:54:46 +0900

    キャンバスを使った表示クラスの追加

---
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -0,0 +1,15 @@
+package com.example.dojo.canvasapp01;
+
+import android.content.Context;
+import android.view.View;
+
+
+
+//コンストラクタ（初期設定）
+public class MyView extends View{
+
+
+    public MyView(Context con) {
+        super(con);
+    }
+}
